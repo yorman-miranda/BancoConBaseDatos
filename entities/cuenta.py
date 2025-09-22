b2584435c392d9e97e3c1692d01abd580e7b8c5d
yorman-miranda <yormanmirandalopez@gmail.com>
2025-09-22 16:05:57 -0500

    comprobaciones de la conexion a la base de datos

--- entities/cuenta.py
+++ entities/cuenta.py
@@ -1,8 +1,10 @@
-
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Float, DateTime, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 from datetime import datetime
-from database.base import Base
+from sqlalchemy.orm import relationship
+from database.config import Base
+
 
 class Cuenta(Base):
     """
@@ -10,17 +12,30 @@ class Cuenta(Base):
     que pertenece a un cliente. Puede ser de tipo
     ahorro, corriente o crédito.
     """
+
     __tablename__ = "cuentas"
 
-    idCuenta = Column(Integer, primary_key=True, autoincrement=True)
+    idCuenta = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     numeroCuenta = Column(String(20), unique=True, nullable=False)
     saldo = Column(Float, default=0.0)
     fechaApertura = Column(DateTime, default=datetime.now)
-    estado = Column(String(20), nullable=False)     
+    estado = Column(String(20), nullable=False)
     tipoCuenta = Column(String(20), nullable=False)
 
-    idCliente = Column(Integer, ForeignKey("clientes.idCliente"))
+    idCliente = Column(UUID(as_uuid=True), ForeignKey("clientes.idCliente"))
+
+    id_usuario_creacion = Column(
+        UUID(as_uuid=True), ForeignKey("users.idUser"), nullable=False
+    )
+    id_usuario_edicion = Column(
+        UUID(as_uuid=True), ForeignKey("users.idUser"), nullable=True
+    )
+    fecha_creacion = Column(DateTime, default=datetime.now, nullable=False)
+    fecha_actualizacion = Column(DateTime, default=None, onupdate=datetime.now)
+
     cliente = relationship("Cliente")
+    usuario_creacion = relationship("User", foreign_keys=[id_usuario_creacion])
+    usuario_edicion = relationship("User", foreign_keys=[id_usuario_edicion])
 
     def __repr__(self):
         return f"<Cuenta {self.numeroCuenta} ({self.tipoCuenta}) - Saldo: {self.saldo}>"
